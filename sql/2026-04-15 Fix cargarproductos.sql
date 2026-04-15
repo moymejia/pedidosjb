c335fad41ddc25999f40021a0877b3d7f0fb2581
danyelcm2 <danyelcm2@gmail.com>
2026-04-15 12:06:39 -0600

    Fix registro

--- sql/2026-04-15 Fix cargarproductos.sql
+++ sql/2026-04-15 Fix cargarproductos.sql
@@ -13,3 +13,8 @@ DROP INDEX idx_producto_idset_talla;
 
 ALTER TABLE pedidosjb_pedidos.producto
 DROP COLUMN idset_talla;
+
+REPLACE INTO pedidosjb_pedidos.temporada
+(idtemporada, nombre, fecha_inicio, fecha_fin, estado, fecha_creacion, usuario_creacion, fecha_modificacion, usuario_modificacion)
+VALUES
+(100, 'DESPACHO INMEDIATO', '0001-01-01', '0001-01-01', 'PROTEGIDO', '2026-04-13 16:47:52', 'admin', NULL, NULL);
