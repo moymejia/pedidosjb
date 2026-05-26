67b865e56234a1bc472c9a41dd773eb5638de14c
danyelcm2 <danyelcm2@gmail.com>
2026-05-26 15:30:46 -0600

    2026-05-26 Se cambio la fecha para algunos pedidos solicitados

---
+++ sql/2026-05-26 CAMBIO DE FECHA PEDIDOS.sql
@@ -0,0 +1,18 @@
+UPDATE pedidosjb_pedidos.pedido
+SET idcliente=52, idtemporada=104, idmarca=4, observaciones_pedido='10% DESCUENTO - CONTADO - 30% ANTICIPO - 70% CONTRA ENTREGA', descuento=0.00, dias_credito=0, monto_subtotal=30564.72, monto_descuento=10.00, monto_total=27508.25, estado='CERRADO', fecha_creacion='2026-05-12 12:49:50', usuario_creacion='jbran', fecha_modificacion='2026-05-12 14:19:33', usuario_modificacion='jbran', fecha_desde='2026-02-01', fecha_hasta='2026-03-31', total_pares=108, email='', idtransporte=2, nopedido='JB-1079', fecha_pedido='2026-10-26'
+WHERE idpedido=88;
+UPDATE pedidosjb_pedidos.pedido
+SET idcliente=52, idtemporada=104, idmarca=15, observaciones_pedido='CONTADO', descuento=0.00, dias_credito=0, monto_subtotal=3960.00, monto_descuento=10.00, monto_total=3564.00, estado='CERRADO', fecha_creacion='2026-05-12 14:44:28', usuario_creacion='jbran', fecha_modificacion='2026-05-12 14:46:12', usuario_modificacion='jbran', fecha_desde='2026-02-01', fecha_hasta='2026-03-31', total_pares=18, email='', idtransporte=2, nopedido='JB-1084', fecha_pedido='2026-10-26'
+WHERE idpedido=89;
+UPDATE pedidosjb_pedidos.pedido
+SET idcliente=37, idtemporada=104, idmarca=4, observaciones_pedido='10% DESCUENTO CONTADO', descuento=0.00, dias_credito=0, monto_subtotal=19082.52, monto_descuento=10.00, monto_total=17174.27, estado='CERRADO', fecha_creacion='2026-05-12 10:35:28', usuario_creacion='jbran', fecha_modificacion='2026-05-12 10:40:13', usuario_modificacion='jbran', fecha_desde='2026-02-01', fecha_hasta='2026-03-31', total_pares=72, email='', idtransporte=2, nopedido='JB-1114', fecha_pedido='2026-10-26'
+WHERE idpedido=87;
+UPDATE pedidosjb_pedidos.pedido
+SET idcliente=37, idtemporada=103, idmarca=4, observaciones_pedido='10% DESCUENO CONTADO', descuento=0.00, dias_credito=0, monto_subtotal=22109.04, monto_descuento=10.00, monto_total=19898.14, estado='CERRADO', fecha_creacion='2026-05-12 09:47:12', usuario_creacion='jbran', fecha_modificacion='2026-05-12 09:55:00', usuario_modificacion='jbran', fecha_desde='2026-04-01', fecha_hasta='2026-05-30', total_pares=99, email='', idtransporte=2, nopedido='JB-1174', fecha_pedido='2026-01-10'
+WHERE idpedido=85;
+UPDATE pedidosjb_pedidos.pedido
+SET idcliente=37, idtemporada=103, idmarca=4, observaciones_pedido='10% DESCUENTO CONTADO', descuento=0.00, dias_credito=0, monto_subtotal=16767.45, monto_descuento=10.00, monto_total=15090.71, estado='CERRADO', fecha_creacion='2026-05-12 09:55:34', usuario_creacion='jbran', fecha_modificacion='2026-05-12 10:00:06', usuario_modificacion='jbran', fecha_desde='2026-04-01', fecha_hasta='2026-05-30', total_pares=63, email='', idtransporte=2, nopedido='JB-1175', fecha_pedido='2026-01-10'
+WHERE idpedido=86;
+UPDATE pedidosjb_pedidos.pedido
+SET idcliente=67, idtemporada=103, idmarca=4, observaciones_pedido='10% DESCUENTO - CONTADO - 50% ANTICIPO - 50% CONTRA ENTREGA', descuento=0.00, dias_credito=0, monto_subtotal=11083.50, monto_descuento=10.00, monto_total=9975.15, estado='CERRADO', fecha_creacion='2026-05-12 16:28:32', usuario_creacion='jbran', fecha_modificacion='2026-05-12 16:34:40', usuario_modificacion='jbran', fecha_desde='2026-04-01', fecha_hasta='2026-05-30', total_pares=36, email='', idtransporte=2, nopedido='JB-1178', fecha_pedido='2026-01-10'
+WHERE idpedido=90;
